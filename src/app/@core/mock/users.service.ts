cdc2abeac4a2e262510192db590d873f38f68bef
arun-augustine-datum <ArunAugustine@datuminnovation.com>
2023-09-10 15:37:15 +0530

    Minor Changes

--- src/app/@core/mock/users.service.ts
+++ src/app/@core/mock/users.service.ts
@@ -8,6 +8,7 @@ export class UserService extends UserData {
   private time: Date = new Date;
 
   private users = {
+    arun: { name: 'Arun', picture: 'assets/images/arun.png' },
     nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
     eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
     jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
